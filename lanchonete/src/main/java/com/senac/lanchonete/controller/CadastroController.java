db7da3947cbd38960ef1595150f0c970a6863f8c
lucas <lucas.sjc87@hotmail.com>
2025-05-28 22:27:56 -0300

    Atualizando classes

--- lanchonete/src/main/java/com/senac/lanchonete/controller/CadastroController.java
+++ lanchonete/src/main/java/com/senac/lanchonete/controller/CadastroController.java
@@ -1,10 +1,29 @@
 
 package com.senac.lanchonete.controller;
 
+import com.senac.lanchonete.service.CadastroService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 
 public class CadastroController {
+    @Autowired
+    private CadastroService cadastroService;
     
+    @GetMapping ("/")
+    public String inicio() {
+        return "index";
+    }
+    
+    @GetMapping("/cadastro")
+    public String exibirFormulario() {
+        return "pedido";
+    }
+    
+    @GetMapping("/lista")
+    public String lista() {
+        return "pedido";
+    }
 }
