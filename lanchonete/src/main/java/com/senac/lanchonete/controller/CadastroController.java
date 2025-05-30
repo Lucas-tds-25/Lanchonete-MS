2f070936d75671f96ca322f6cd49aa39dee3d2d8
lucas <lucas.sjc87@hotmail.com>
2025-05-29 21:13:00 -0300

    Implementando mais códigos

--- lanchonete/src/main/java/com/senac/lanchonete/controller/CadastroController.java
+++ lanchonete/src/main/java/com/senac/lanchonete/controller/CadastroController.java
@@ -1,10 +1,14 @@
 
 package com.senac.lanchonete.controller;
 
+import com.senac.lanchonete.model.Cadastro;
 import com.senac.lanchonete.service.CadastroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 
@@ -18,8 +22,15 @@ public class CadastroController {
     }
     
     @GetMapping("/cadastro")
-    public String exibirFormulario() {
+    public String exibirFormulario(Model model) {
+        model.addAttribute("cadastro", new Cadastro());
         return "pedido";
+    }
+    
+    @PostMapping ("/gravar")
+    public String processarFormulario(@ModelAttribute Cadastro cadastro) {
+        cadastroService.salvar(cadastro);
+        return "redirect:/pedido";
     }
     
     @GetMapping("/lista")
